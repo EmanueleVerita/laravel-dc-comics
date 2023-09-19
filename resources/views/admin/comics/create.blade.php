58353c9b9ebb390b87408efa8ef990621f2e4276
EmanueleVerita <emanuele.verita@gmail.com>
2023-09-19 18:43:36 +0200

    modifica validation

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -27,32 +27,62 @@
 
                 <div class="mb-3">
                     <label for="title" class="form-label">Title <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" value="{{ old('title') }}" id="title" name="title" placeholder="Write a Title..." required>
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}" id="title" name="title" placeholder="Write a Title..." required>
+                    @error('title')
+                        <div class="alert alert-danger my-2">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="thumb" class="form-label">Thumb <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="thumb" name="thumb" placeholder="Write a Thumble..." required>
+                    <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" placeholder="Write a Thumble..." required>
+                    @error('thumb')
+                    <div class="alert alert-danger my-2">
+                        {{ $message }}
+                    </div>
+                @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="type" class="form-label">Type <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" value="{{ old('type') }}" id="type" name="type" placeholder="Write a Type..." required>
+                    <input type="text" class="form-control @error('type') is-invalid @enderror" value="{{ old('type') }}" id="type" name="type" placeholder="Write a Type..." required>
+                    @error('type')
+                    <div class="alert alert-danger my-2">
+                        {{ $message }}
+                    </div>
+                @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="price" class="form-label">Price <span class="text-danger">*</span></label>
-                    <input type="number" class="form-control" value="{{ old('price') }}" id="price" name="price" placeholder="Write a Price..." required>
+                    <input type="number" class="form-control @error('price') is-invalid @enderror" value="{{ old('price') }}" id="price" name="price" placeholder="Write a Price..." required>
+                    @error('price')
+                    <div class="alert alert-danger my-2">
+                        {{ $message }}
+                    </div>
+                @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="series" class="form-label">Series <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" value="{{ old('serie') }}" id="series" name="series" placeholder="Write a Series..." required>
+                    <input type="text" class="form-control @error('series') is-invalid @enderror" value="{{ old('serie') }}" id="series" name="series" placeholder="Write a Series..." required>
+                    @error('series')
+                    <div class="alert alert-danger my-2">
+                        {{ $message }}
+                    </div>
+                @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="sale_date" class="form-label">Sale Date <span class="text-danger">*</span></label>
-                    <input type="date" class="form-control" value="{{ old('sale_date') }}" id="sale_date" name="sale_date" placeholder="Write Sale Date..." required>
+                    <input type="date" class="form-control @error('sale_date') is-invalid @enderror" value="{{ old('sale_date') }}" id="sale_date" name="sale_date" placeholder="Write Sale Date..." required>
+                    @error('sale_date')
+                    <div class="alert alert-danger my-2">
+                        {{ $message }}
+                    </div>
+                @enderror
                 </div>
 
                 <div class="mb-3">
@@ -67,7 +97,12 @@
 
                 <div class="mb-3">
                     <label for="description" class="form-label">Description <span class="text-danger">*</span></label>
-                    <textarea name="form-control" id="description" name="description" rows="3" required></textarea>
+                    <textarea name="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="3" required></textarea>
+                    @error('description')
+                    <div class="alert alert-danger my-2">
+                        {{ $message }}
+                    </div>
+                @enderror
                 </div>
 
                 <div class="text-center">
